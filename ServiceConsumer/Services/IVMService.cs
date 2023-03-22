17253bf99977115df3414ade67cd59e34d9f884b
Ayokunle Olowoniwa <ayo.olowoniwa@hometrack.com>
2023-03-22 19:10:08 +0000

    Added ServiceComsumer interface and service

---
+++ ServiceConsumer/Services/IVMService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceConsumer.Services
+{
+    public interface IVMService
+    {
+        string Create();
+        string GetStatus(string requestId);
+    }
+}
---
+++ ServiceConsumer/Services/VMService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceConsumer.Services
+{
+    public class VMService
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        private static string _vmServiceUrl = "https://localhost:7035/api/vm/";
+
+        public string Create()
+        {
+            HttpResponseMessage response = _httpClient.PostAsync(_vmServiceUrl + "create", null).Result;
+            if(!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            string requestId = response.Content.ReadAsStringAsync().Result;
+            return requestId;
+        }
+
+        public string GetStatus(string requestId)
+        {
+            HttpResponseMessage response = _httpClient.GetAsync(_vmServiceUrl + "status?requestId=" + requestId).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            string status = response.Content.ReadAsStringAsync().Result;
+            return status;
+        }
+    }
+}
